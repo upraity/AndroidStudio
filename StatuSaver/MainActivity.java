44cfe043ff10e1a34e5a03e8fc49f6ca5a0dc299
Dj <134566303+upraity@users.noreply.github.com>
2025-02-11 11:09:47 +0530

    Add files via upload

---
+++ StatuSaver/MainActivity.java
@@ -0,0 +1,50 @@
+package com.djupraity.status;
+
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.djupraity.status.databinding.ActivityMainBinding;
+import com.djupraity.status.ui.dashboard.DashboardFragment;
+import com.djupraity.status.ui.home.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation, R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+    private void loadTheme() {
+        SharedPreferences preferences = getSharedPreferences("AppTheme", MODE_PRIVATE);
+        int mode = preferences.getInt("theme_mode", AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(mode);
+    }
+
+}
